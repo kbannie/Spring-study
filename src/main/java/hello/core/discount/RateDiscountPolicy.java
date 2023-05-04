608ec631fb56ff742c2a3922134f05d1fa9522ad
kbannie <kabin2205@gmail.com>
2023-05-04 16:55:27 +0900

    @Autowired 필드 명, @Qualifier, @Primary

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,10 +2,13 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy{
     private int disountPercent=10;
 
